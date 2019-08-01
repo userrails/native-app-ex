5782be48b7a4c78f4fceaf4ebd96bffe63a44d69
Shiv Raj <userrails@gmail.com>
2019-08-01 16:06:22 +0545

    top space increases

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    top: 22,
     backgroundColor: '#DDD',
     flexDirection: 'row',
     alignItems: 'center',
