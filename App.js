a8e1630809770eca1f5ccfc57130556c4ae277f9
Shiv Raj <userrails@gmail.com>
2019-08-01 14:18:49 +0545

    new font color styles added

--- App.js
+++ App.js
@@ -8,11 +8,11 @@ import {
 export default function App() {
   return (
     <View style={styles.container}>
-        <Text style={[styles.defaultText, styles.selectedText]}>Shiv</Text>
-        <Text style={[styles.defaultText, styles.selectedText]}>Raj</Text>
-        <Text style={[styles.defaultText, styles.selectedText]}>Badu</Text>
-        <Text style={[styles.defaultText, styles.selectedText]}>Prz</Text>
-        <Text style={[styles.defaultText, styles.nepaliText]}>शिव राज बडु</Text>
+        <Text style={[styles.defaultText, styles.selectedText, styles.fontBlue]}>Hello World</Text>
+        <Text style={[styles.defaultText, styles.selectedText, styles.fontBlue]}>Shiv</Text>
+        <Text style={[styles.defaultText, styles.selectedText, styles.fontBlue]}>Raj</Text>
+        <Text style={[styles.defaultText, styles.selectedText, styles.fontBlue]}>Badu</Text>
+        <Text style={[styles.defaultText, styles.nepaliText, styles.fontWhite]}>शिव राज बडु</Text>
     </View>
   );
 }
@@ -26,22 +26,25 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around', // move content to center of screen, other options: flex-start, flex-end, space-between, space-around
   },
   defaultText: {
-    fontSize: 22,
+    fontSize: 12,
     padding: 10,
     margin: 5,
-    color: 'black',
     borderWidth: StyleSheet.hairlineWidth
   },
   selectedText: {
     backgroundColor: 'yellow',
-    color: 'blue',
     fontWeight: 'bold',
     alignSelf: 'flex-start'
   },
   nepaliText: {
     backgroundColor: 'blue',
-    color: 'white',
     fontWeight: 'bold',
     alignSelf: 'flex-start'
+  },
+  fontBlue: {
+    color: 'blue'
+  },
+  fontWhite: {
+    color: 'white'
   }
 });
