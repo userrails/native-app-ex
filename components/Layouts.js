88cd29ea92222988fa2a4b672e38d7de71a993bb
Shiv Raj <userrails@gmail.com>
2019-08-21 11:34:55 +0545

    new component Lyaouts is created

---
+++ components/Layouts.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import { View, StyleSheet, Button, Text } from 'react-native';
+
+class Layouts extends Component {
+    render () {
+        return (
+            <View>
+                <View>
+                    <Text style={[styles.colorRed, styles.button]}>Red</Text>
+                    <Text style={[styles.colorYellow, styles.button]}>Yellow</Text>
+                    <Text style={[styles.colorGreen, styles.button]}>Green</Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    colorRed: {
+        backgroundColor: 'red'
+    },
+    colorYellow: {
+        backgroundColor: 'yellow'
+    },
+    colorGreen: {
+        backgroundColor: 'green'
+    },
+    button: {
+        margin: 10,
+        padding: 10,
+        borderWidth: 2,
+        borderRadius: 10,
+        alignSelf: 'stretch'
+    }
+})
+
+export default Layouts;
