36d4a7fc60872de91c91bceb43135d40f7c4b724
Shiv Raj <userrails@gmail.com>
2019-08-23 10:38:29 +0545

    AlignContentFlexStart properties added

---
+++ components/AlignContentFlexStart.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native'
+
+class AlignContentFlexStart extends Component {
+    render () {
+        return (
+            <View>
+                <View style={{ flexWrap: 'wrap', flexDirection: 'column', alignContent: 'flex-start' }}>
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                    <View style={styles.boxStyle} />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    boxStyle: {
+        height: 50, 
+        width: 50, 
+        borderWidth: 1, 
+        backgroundColor: 'orange', 
+        marginBottom: 5
+      },
+})
+
+export default AlignContentFlexStart;
